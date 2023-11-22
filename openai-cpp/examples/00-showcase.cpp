2471e296c73a1a9bcfe4266aa8019bc6af776040
kyeongchan317 <jac07171@gmail.com>
2023-11-23 00:07:39 +0900

    update

--- openai-cpp/examples/00-showcase.cpp
+++ openai-cpp/examples/00-showcase.cpp
@@ -1,92 +1,213 @@
 #include "openai.hpp"
+#include "project.h"
 
 #include <iostream>
 #include <string>
-
-enum
-{
-    blue = 0,
-    red = 1,
-};
-
-enum
-{
-    top = 0,
-    jungle = 1,
-    mid = 2,
-    adcarry = 3,
-    supporter = 4,
-};
-
-enum
-{
-    exit = 0,
-    next = 1,
-    command1 = 2,
-};
-
-namespace champion
-{
-    class Champion
-    {
-    public:
-    private:
-        std::string name;
-        int position; // 0: top, 1: jungle, 2: mid, 3: adcarry, 4: supporter
-    };
-} // namespace champion
 
 int main()
 {
     openai::start(); // Will use the api key provided by `OPENAI_API_KEY` environment variable
                      // openai::start("your_API_key", "optional_organization"); // Or you can handle it yourself
 
-    auto completion = openai::completion().create(R"(
-                         {
-                             "model": "text-davinci-003",
-                             "prompt": "롤 챔피언의 수",
-                             "max_tokens": 10,
-                             "temperature": 0
-                         }
-                         )"_json); // Using user-defined (raw) string literals
-    std::cout << "Response is:\n"
-              << completion.dump(2) << '\n';
+    /*
+        auto completion = openai::completion().create(R"(
+                             {
+                                 "model": "text-davinci-003",
+                                 "prompt": "how many champions in league of legends game?",
+                                 "max_tokens": 20,
+                                 "temperature": 0
+                             }
+                             )"_json); // Using user-defined (raw) string literals
+        std::cout << "Response is:\n"
+                  << completion.dump(2) << '\n'
 
-    auto image = openai::image().create({{"prompt", "A logo with a cello in a heart"},
-                                         {"n", 1},
-                                         {"size", "512x512"}}); // Using initializer lists
-    std::cout << "Image URL is: " << image["data"][0]["url"] << '\n';
+                  << completion["choices"][0]["text"] << '\n';
+
+        auto image = openai::image().create({{"prompt", "A logo with a cello in a heart"},
+                                             {"n", 1},
+                                             {"size", "512x512"}}); // Using initializer lists
+        std::cout << "Image URL is: "
+                  << image.dump(2) << '\n'
+                  << image["data"][0]["url"] << '\n';
+    */
 
     while (1)
     {
-        std::cout << "Choose Blue Team or Red Team [b/r]: \n"
-                  << std::endl;
-
         char team_color; // blue: b or red: r
+        char is_my_turn;
+        int myPosition;
+        int ally, enemy;
+
+        std::cout << "Choose Blue Team or Red Team [b/r]: ";
         std::cin >> team_color;
 
-        if (team_color == 'b')
+        if (team_color == 'b' || team_color == 'B')
         {
+            ally = blue;
+            enemy = red;
+        }
+        else if (team_color == 'r' || team_color == 'R')
+        {
+            ally = red;
+            enemy = blue;
         }
         else
         {
+            std::cout << "Wrong input : You can input B/b or R/r" << std::endl;
+            continue;
         }
 
-        int next_command;
+        std::cout << "Top: 0, Jungle: 1, Mid: 2, ADC: 3, Supporter: 4" << std::endl;
+        std::cout << "What is your position?" << std::endl;
+        std::cin >> myPosition;
+
+        champion::Ban_Champion Ban_List;
+        champion::Ally_Champion Ally_List;
+        champion::Enemy_Champion Enemy_List;
+
+        // 1. input 10 ban list
+        for (int i = 0; i < 10; i++)
+        {
+            std::string name;
+            std::cout << "Input banned champions "
+                      << "[" << i + 1 << "]"
+                      << ": ";
+            std::cin >> name;
+
+            Ban_List.set_name(name);
+        }
+
+        // 2. check your pick turn or not
+        int turn;
+        for (turn = 0; turn < 10; turn++)
+        {
+            if (pick_order[turn] == ally)
+            {
+                std::cout << "Is it your pick turn? [y/n]: ";
+                std::cin >> is_my_turn;
+                // 2-1. if not, append pick list
+                if (is_my_turn == 'n' || is_my_turn == 'N')
+                {
+                    std::string name;
+                    std::cout << "Input picked champion: ";
+                    std::cin >> name;
+
+                    int position;
+                    std::cout << "Top: 0, Jungle: 1, Mid: 2, ADC: 3, Supporter: 4" << std::endl;
+                    std::cout << "Input poistion: ";
+                    std::cin >> position;
+
+                    Ally_List.set_name(name);
+                    Ally_List.set_position(position);
+                }
+                // 3. my turn -> suggest with ban & pick list
+                else if (is_my_turn == 'y' || is_my_turn == 'Y')
+                {
+                    std::string ban_Str;
+                    std::string ally_Str;
+                    std::string enemy_Str;
+                    std::string question;
+
+                    ban_Str = "Banned Champions: ";
+                    ban_Str += Ban_List.info_str();
+                    ally_Str = "Ally Champions: ";
+                    ally_Str += Ally_List.info_str();
+                    enemy_Str = "Enemy Champions: ";
+                    enemy_Str += Enemy_List.info_str();
+
+                    question = ban_Str + ally_Str + enemy_Str + ". ";
+                    question += "Which champion would be the best for ";
+                    question += pos_str[myPosition];
+                    question += " ?";
+
+                    std::cout << question << std::endl;
+
+                    std::cout << completion(question, 300, 0) << std::endl;
+
+                    break;
+                }
+                else
+                {
+                    std::cout << "Wrong input : You can input Y/y or N/n";
+                    turn--;
+                    continue;
+                }
+            }
+            else if (pick_order[turn] == enemy)
+            {
+
+                std::string name;
+                std::cout << "Input picked champion: ";
+                std::cin >> name;
+
+                int position;
+                std::cout << "Top: 0, Jungle: 1, Mid: 2, ADC: 3, Supporter: 4" << std::endl;
+                std::cout << "Input poistion: ";
+                std::cin >> position;
+
+                Enemy_List.set_name(name);
+                Enemy_List.set_position(position);
+            }
+
+            Ban_List.print();
+            Ally_List.print();
+            Enemy_List.print();
+        }
+        for (; turn < 10; turn++)
+        {
+            // 4. append pick list
+            if (pick_order[turn] == ally)
+            {
+                std::string name;
+                std::cout << "Input picked champion: ";
+                std::cin >> name;
+
+                int position;
+                std::cout << "Top: 0, Jungle: 1, Mid: 2, ADC: 3, Supporter: 4" << std::endl;
+                std::cout << "Input poistion: ";
+                std::cin >> position;
+
+                Ally_List.set_name(name);
+                Ally_List.set_position(position);
+            }
+            else
+            {
+
+                std::string name;
+                std::cout << "Input picked champion: ";
+                std::cin >> name;
+
+                int position;
+                std::cout << "Top: 0, Jungle: 1, Mid: 2, ADC: 3, Supporter: 4" << std::endl;
+                std::cout << "Input poistion: ";
+                std::cin >> position;
+
+                Enemy_List.set_name(name);
+                Enemy_List.set_position(position);
+            }
+        }
+
+        // 5. 상대법 with my 상대 라인 champion
+
+        int command;
         do
         {
-            std::cin >> next_command;
-            switch (next_command)
+            std::cout << "exit: 0, next: 1, command: 2" << std::endl;
+            std::cout << "Enter Command: ";
+            std::cin >> command;
+            switch (command)
             {
             case command1:
                 break;
             case next: // go to selecting team color
                 break;
-            case exit: // exit program
-                return;
+            case done: // exit program
+                return 0;
             default:
                 break;
             }
-        } while (next_command != next);
+        } while (command != next);
     }
+
+    return 0;
 }
---
+++ openai-cpp/examples/project.h
@@ -0,0 +1,184 @@
+#include "openai.hpp"
+#include <string>
+#include <vector>
+
+enum
+{
+    blue = 0,
+    red = 1,
+};
+
+enum
+{
+    top = 0,
+    jungle = 1,
+    mid = 2,
+    adcarry = 3,
+    supporter = 4,
+};
+
+enum
+{
+    done = 0,
+    next = 1,
+    command1 = 2,
+};
+
+int pick_order[10] = {blue, red, red, blue, blue, red, red, blue, blue, red};
+std::string pos_str[5] = {"Top", "Jungle", "Mid", "ADC", "Supporter"};
+
+namespace champion
+{
+    class Champion
+    {
+    public:
+        Champion() {}
+        virtual ~Champion() {}
+
+        void set_name(const std::string &new_name) { names.push_back(new_name); }
+        std::string get_name(int i) const { return names[i]; }
+
+        std::string info_str()
+        {
+            std::string str;
+            for (const auto &name : names)
+            {
+                str += name;
+                str += ", ";
+            }
+            return str;
+        }
+        virtual void print() const
+        {
+            for (const auto &name : names)
+            {
+                std::cout << name << ", ";
+            }
+        }
+
+    protected:
+        std::vector<std::string> names;
+    };
+
+    class Ban_Champion : public Champion
+    {
+    public:
+        Ban_Champion() : Champion() {}
+        ~Ban_Champion() {}
+
+        void print() const
+        {
+            std::cout << "Ban:\t";
+            Champion::print();
+            std::cout << std::endl;
+        }
+    };
+
+    class Ally_Champion : public Champion
+    {
+    public:
+        Ally_Champion() : Champion() {}
+        ~Ally_Champion() {}
+
+        void set_position(int new_position) { positions.push_back(new_position); }
+        int get_position(int i) { return positions[i]; }
+
+        void print() const
+        {
+            std::cout << "Ally:\t";
+            for (size_t i = 0; i < names.size(); i++)
+            {
+                std::cout << "Name: " << names[i] << ", Position: " << pos_str[positions[i]] << "/ ";
+            }
+            std::cout << std::endl;
+        }
+
+    private:
+        std::vector<int> positions; // 0: top, 1: jungle, 2: mid, 3: adcarry, 4: supporter
+    };
+
+    class Enemy_Champion : public Champion
+    {
+    public:
+        Enemy_Champion() : Champion() {}
+        ~Enemy_Champion() {}
+
+        void set_position(int new_position) { positions.push_back(new_position); }
+        int get_position(int i) { return positions[i]; }
+
+        void print() const
+        {
+            std::cout << "Enemy:\t";
+            for (size_t i = 0; i < names.size(); i++)
+            {
+                std::cout << "Name: " << names[i] << ", Position: " << pos_str[positions[i]] << "/ ";
+            }
+            std::cout << std::endl;
+        }
+
+    private:
+        std::vector<int> positions; // 0: top, 1: jungle, 2: mid, 3: adcarry, 4: supporter
+    };
+} // namespace champion
+
+std::string completion(std::string prompt)
+{
+    nlohmann::json input;
+
+    input["model"] = "text-davinci-003";
+    input["prompt"] = prompt;
+    input["max_tokens"] = 7;
+    input["temperature"] = 0;
+
+    auto completion = openai::completion().create(input);
+
+    return (std::string)completion["choices"][0]["text"];
+}
+
+std::string completion(std::string prompt, int max_tokens)
+{
+    nlohmann::json input;
+
+    input["model"] = "text-davinci-003";
+    input["prompt"] = prompt;
+    input["max_tokens"] = max_tokens;
+    input["temperature"] = 0;
+
+    auto completion = openai::completion().create(input);
+
+    return (std::string)completion["choices"][0]["text"];
+}
+
+std::string completion(std::string prompt, int max_tokens, float temperature)
+{
+    nlohmann::json input;
+
+    input["model"] = "text-davinci-003";
+    input["prompt"] = prompt;
+    input["max_tokens"] = max_tokens;
+    input["temperature"] = temperature;
+
+    auto completion = openai::completion().create(input);
+
+    return (std::string)completion["choices"][0]["text"];
+}
+
+// 1. blue 1 ban suggest
+// 2. red 1 ban by computer (with current ban list)(random)
+// 3. blue 2 ban suggest (with current ban list)
+// 4. red 2 ban by computer (with current ban list)(random)
+// 5. blue 3 ban suggest (with current ban list)
+// 6. red 3 ban by computer (with current ban list)(random)
+
+// 7. blue 1 pick suggest (with current ban list)
+// 8. red 1 pick, 2 pick by computer (with current ban list & pick list)(random)
+// 9. blue 2 pick, 3 pick suggest (with current ban list & pick list)
+// 10. red 3 pick by computer (with current ban list & pick list)(random)
+
+// 11. red 4 ban by computer (with current ban list & pick list)(random)
+// 12. blue 4 ban, 5 ban suggest (with current ban list & pick list)
+// 13. red 5 ban by computer (with current ban list & pick list)(random)
+
+// 14. red 4 pick by computer (with current ban list & pick list)(random)
+// 15. blue 4 pick, 5 pick suggest (with current ban list & pick list)
+// 16. red 5 pick by computer (with current ban list & pick list)(random)
