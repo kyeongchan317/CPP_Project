9189affdc1da0f46c979d6ba678becc6bebdfa4d
kyeongchan317 <jac07171@gmail.com>
2023-11-27 17:22:29 +0900

    kc jh update

--- openai-cpp/examples/00-showcase.cpp
+++ openai-cpp/examples/00-showcase.cpp
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <string>
-
 int main()
 {
     openai::start(); // Will use the api key provided by `OPENAI_API_KEY` environment variable
@@ -33,21 +32,21 @@ int main()
 
     while (1)
     {
-        char team_color; // blue: b or red: r
-        char is_my_turn;
-        int myPosition;
+        std::string team_color; // blue: b or red: r
+        std::string is_my_turn;
+        std::string myPosition;
         std::string myChampion;
         int ally, enemy;
 
         std::cout << "Choose Blue Team or Red Team [b/r]: ";
-        std::cin >> team_color;
+        std::getline(std::cin, team_color);
 
-        if (team_color == 'b' || team_color == 'B')
+        if (team_color == "b" || team_color == "B")
         {
             ally = blue;
             enemy = red;
         }
-        else if (team_color == 'r' || team_color == 'R')
+        else if (team_color == "r" || team_color == "R")
         {
             ally = red;
             enemy = blue;
@@ -58,9 +57,9 @@ int main()
             continue;
         }
 
-        std::cout << "1. Top\t\t2. Jungle\t3. Mid\t\t4. ADC\t\t5. Supporter" << std::endl;
+        std::cout << "\n1. Top\t\t2. Jungle\t3. Mid\t\t4. ADC\t\t5. Supporter" << std::endl;
         std::cout << "What is your position?" << std::endl;
-        std::cin >> myPosition;
+        std::getline(std::cin, myPosition);
 
         champion::Ban_Champion Ban_List;
         champion::Ally_Champion Ally_List;
@@ -73,7 +72,7 @@ int main()
             std::cout << "Input banned champions "
                       << "[" << i + 1 << "]"
                       << ": ";
-            std::cin >> name;
+            std::getline(std::cin, name);
 
             Ban_List.set_name(name);
         }
@@ -85,24 +84,25 @@ int main()
             if (pick_order[turn] == ally)
             {
                 std::cout << "Is it your pick turn? [y/n]: ";
-                std::cin >> is_my_turn;
+                std::getline(std::cin, is_my_turn);
                 // 2-1. if not, append pick list
-                if (is_my_turn == 'n' || is_my_turn == 'N')
+                if (is_my_turn == "n" || is_my_turn == "N")
                 {
                     std::string name;
-                    std::cout << "Input picked champion: ";
-                    std::cin >> name;
+                    std::cout << "Ally: "
+                              << "Input picked champion: ";
+                    std::getline(std::cin, name);
 
-                    int position;
+                    std::string position;
                     std::cout << "1. Top\t\t2. Jungle\t3. Mid\t\t4. ADC\t\t5. Supporter" << std::endl;
                     std::cout << "Input poistion: ";
-                    std::cin >> position;
+                    std::getline(std::cin, position);
 
                     Ally_List.set_name(name);
-                    Ally_List.set_position(position);
+                    Ally_List.set_position(stoi(position));
                 }
                 // 3. my turn -> suggest with ban & pick list
-                else if (is_my_turn == 'y' || is_my_turn == 'Y')
+                else if (is_my_turn == "Y" || is_my_turn == "y")
                 {
                     std::string ban_Str;
                     std::string ally_Str;
@@ -116,27 +116,29 @@ int main()
                     enemy_Str = "Enemy Champions: ";
                     enemy_Str += Enemy_List.info_str();
 
-                    question = ban_Str + ally_Str + enemy_Str + ". ";
+                    question = "In league of legend, ";
+                    question += ban_Str + ally_Str + enemy_Str + ". ";
                     question += "Which champion would be the best for ";
-                    question += pos_str[myPosition];
-                    question += " ?";
+                    question += pos_str[stoi(myPosition)];
+                    question += " in this situation? Without picked or banned champions.";
 
                     std::cout << question << std::endl;
 
-                    // std::cout << completion(question, 300, 0) << std::endl;
+                    std::cout << completion(question, 300, 0) << std::endl;
 
-                    std::cout << "Input picked champion: ";
-                    std::cin >> myChampion;
+                    std::cout << "Me: "
+                              << "Input picked champion: ";
+                    std::getline(std::cin, myChampion);
 
                     Ally_List.set_name(myChampion);
-                    Ally_List.set_position(myPosition);
+                    Ally_List.set_position(stoi(myPosition));
 
                     turn++;
                     break;
                 }
                 else
                 {
-                    std::cout << "Wrong input : You can input Y/y or N/n";
+                    std::cout << "Wrong input : You can input Y/y or N/n" << std::endl;
                     turn--;
                     continue;
                 }
@@ -145,16 +147,17 @@ int main()
             {
 
                 std::string name;
-                std::cout << "Input picked champion: ";
-                std::cin >> name;
+                std::cout << "Enemy: "
+                          << "Input picked champion: ";
+                std::getline(std::cin, name);
 
-                int position;
+                std::string position;
                 std::cout << "1. Top\t\t2. Jungle\t3. Mid\t\t4. ADC\t\t5. Supporter" << std::endl;
                 std::cout << "Input poistion: ";
-                std::cin >> position;
+                std::getline(std::cin, position);
 
                 Enemy_List.set_name(name);
-                Enemy_List.set_position(position);
+                Enemy_List.set_position(stoi(position));
             }
 
             Ban_List.print();
@@ -167,31 +170,33 @@ int main()
             if (pick_order[turn] == ally)
             {
                 std::string name;
-                std::cout << "Input picked champion: ";
-                std::cin >> name;
+                std::cout << "Ally: "
+                          << "Input picked champion: ";
+                std::getline(std::cin, name);
 
-                int position;
+                std::string position;
                 std::cout << "1. Top\t\t2. Jungle\t3. Mid\t\t4. ADC\t\t5. Supporter" << std::endl;
                 std::cout << "Input poistion: ";
-                std::cin >> position;
+                std::getline(std::cin, position);
 
                 Ally_List.set_name(name);
-                Ally_List.set_position(position);
+                Ally_List.set_position(stoi(position));
             }
             else
             {
 
                 std::string name;
-                std::cout << "Input picked champion: ";
-                std::cin >> name;
+                std::cout << "Enemy: "
+                          << "Input picked champion: ";
+                std::getline(std::cin, name);
 
-                int position;
+                std::string position;
                 std::cout << "1. Top\t\t2. Jungle\t3. Mid\t\t4. ADC\t\t5. Supporter" << std::endl;
                 std::cout << "Input poistion: ";
-                std::cin >> position;
+                std::getline(std::cin, position);
 
                 Enemy_List.set_name(name);
-                Enemy_List.set_position(position);
+                Enemy_List.set_position(stoi(position));
             }
 
             Ban_List.print();
@@ -201,30 +206,50 @@ int main()
 
         // 5. 상대법 with my 상대 라인 champion
 
-        int command;
+        std::string command;
         do
         {
-            std::cout << "1. Exit\t\t2. Next Game\t3. How to deal with my opponent\t\t4. Create a new champion" << std::endl;
+            std::cout << "\n1. Exit\t\t2. Next Game\t3. How to deal with my opponent\n4. How to deal with enemy team\t5. Create a new champion" << std::endl;
             std::cout << "Enter Command: ";
-            std::cin >> command;
+            std::getline(std::cin, command);
 
             std::string question;
             std::string answer;
+            std::string champion_info;
             std::string image_url;
-            switch (command)
+            std::string make_story;
+            std::string make_image;
+            std::string make_skin;
+            std::string style;
+            switch (stoi(command))
             {
             case command1:
-                question += "How to deal with ";
-                question += Enemy_List.get_myOpponent(myPosition);
+                question = "In league of legend, How to deal with ";
+                question += Enemy_List.get_myOpponent(stoi(myPosition));
                 question += " with ";
                 question += myChampion;
                 question += "?";
 
                 std::cout << question << std::endl;
-                // answer = completion(question, 300, 0);
-                // std::cout << answer << std::endl;
+                answer = completion(question, 300, 0);
+                std::cout << answer << std::endl;
                 break;
             case command2:
+                question = "In league of legend, ";
+                question += "Banned Champions: ";
+                question += Ban_List.info_str();
+                question += "Ally Champions: ";
+                question += Ally_List.info_str();
+                question += "Enemy Champions: ";
+                question += Enemy_List.info_str();
+                question += "How to deal with enemy team in this situation?";
+
+                std::cout << question << std::endl;
+                answer = completion(question, 300, 0);
+                std::cout << answer << std::endl;
+                break;
+            case command3:
+                question = "In league of legend, ";
                 question += "Banned Champions: ";
                 question += Ban_List.info_str();
                 question += "Ally Champions: ";
@@ -234,25 +259,44 @@ int main()
                 question += "Create a new champion for instead of ";
                 question += myChampion;
                 question += ".";
-                std::cout << question << std::endl;
-                // answer = completion(question, 300, 0);
-                // std::cout << answer << std::endl;
 
-                question = answer;
-                question += "Make a image of the champion.";
                 std::cout << question << std::endl;
-                // image_url = imageURL(question);
-                // std::cout << image_url << std::endl;
+                champion_info = completion(question, 300, 0);
+                std::cout << champion_info << std::endl;
 
+                std::cout << "Do you want to get story of the champion?[Y/N]: ";
+                std::getline(std::cin, make_story);
+
+                if (make_story == "Y" || make_story == "y")
+                {
+                    question = champion_info;
+                    question += "Make a story of the champion.";
+                    std::cout << question << std::endl;
+                    answer = completion(question, 300, 0);
+                    std::cout << answer << std::endl;
+                }
+
+                std::cout << "Do you want to make image of the champion?[Y/N]: ";
+                std::getline(std::cin, make_image);
+
+                if (make_image == "Y" || make_image == "y")
+                {
+                    question = champion_info;
+                    question += " Make a image of the champion.";
+                    std::cout << question << std::endl;
+                    image_url = imageURL(question);
+                    std::cout << image_url << std::endl;
+                }
                 break;
             case next: // go to selecting team color
                 break;
             case done: // exit program
                 return 0;
             default:
+                std::cout << "Wrong input : You can input number" << std::endl;
                 break;
             }
-        } while (command != next);
+        } while (stoi(command) != next);
     }
 
     return 0;
